2afdcc0f01b8877f5342809b1f8b86fcea045c73
NACXIIX <nicocordano@hotmail.com>
2024-06-11 17:59:55 -0300

    je

--- UTN/PROYECTO 2 PROGRA/cordano_programa1.cpp
+++ UTN/PROYECTO 2 PROGRA/cordano_programa1.cpp
@@ -67,8 +67,8 @@ void llenarMatriz(){
 
         switch (respuesta){
         case 1:
-            for(int i = 0; i<3; i++){
-                for (int j = 0; j<3; j++){
+            for(int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++){
                     matriz[i][j] = rand() % 100+1;
                 }
             }
