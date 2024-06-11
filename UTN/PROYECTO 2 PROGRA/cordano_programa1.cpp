41bc6e73adb96d2c810ac870a950d9d90e38fa29
NACXIIX <nicocordano@hotmail.com>
2024-06-11 12:34:07 -0300

    Se siguen agregando cosas

--- UTN/PROYECTO 2 PROGRA/cordano_programa1.cpp
+++ UTN/PROYECTO 2 PROGRA/cordano_programa1.cpp
@@ -1,53 +1,59 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
 
 //Funciones globales
 int matriz[3][3];
 
 //Prototipo de funcion
+int menu();
 void llenarMatriz();
 void mostrarMatriz();
 int buscarValor();
 int devolverValor();
 int ordenarMatriz();
 
-// programa principal
+// PROGRAMA PRINCIPAL
 int main(){
-    int opcion;
+    int op = menu();
 
-    do{
-        cout << "\t\t--- MENU INTERACTIVO ---" << '\n' << endl;
-        cout << "Elija una opción." << endl;
-        cout << "1. Cargar la matriz" << endl;
-        cout << "2. Mostrar los valores de la matriz" << endl;
-        cout << "3. Buscar un valor específico ingresado por el usuario dentro de la matriz" << endl;
-        cout << "4. Devolver el valor máximo o mínimo de la matriz" << endl;
-        cout << "5. Ordenar los valores de la matriz de forma ascendente o descendente" << endl;
-        cout << "6. Salir del programa" << endl;
-        cin >> opcion;
-
-        switch(opcion){
-            case 1: 
-                llenarMatriz();
-                break;
-            case 2:
-                mostrarMatriz();
-        }
-
-        if (opcion == 1){
+    switch(op){
+        case 1: 
+            llenarMatriz();
+            break;
+        case 2:
             mostrarMatriz();
-        } else {
-            cout << "No esta la matriz cargada correctamente";
-        }
-    } while (opcion <=0 || opcion > 6);
+            break;
+        case 6:
+            cout << "Saliste del menu" << endl;
+            break;
+    }
+
     return 0;
 }
 
 
-// 
+// MENU INTERACTIVO
+int menu(){
+    int opcion;
+
+    do{
+        cout << "\t\t--- MENU INTERACTIVO ---" << '\n' << endl;
+        cout << "Elija una opcion.\n" << endl;
+        cout << "1. Cargar la matriz" << endl;
+        cout << "2. Mostrar los valores de la matriz" << endl;
+        cout << "3. Buscar un valor especifico ingresado por el usuario dentro de la matriz" << endl;
+        cout << "4. Devolver el valor maximo o minimo de la matriz" << endl;
+        cout << "5. Ordenar los valores de la matriz de forma ascendente o descendente" << endl;
+        cout << "6. Salir del programa" << endl;
+        cin >> opcion;
+    } while (opcion <=0 || opcion > 6);
+
+    return opcion;
+}
+
+// CARGAR LA MATRIZ
 void llenarMatriz(){
     srand(time(NULL));
     int respuesta;
@@ -81,9 +87,14 @@ void llenarMatriz(){
 
     } while (respuesta != 1 && respuesta != 2 && respuesta !=3);
 
+    main();
 }
 
+// MOSTRAR MATRIZ
 void mostrarMatriz(){
+    int volver;
+
+    cout << "-------------------";
     for(int i = 0; i < 3; i ++){
         cout << endl;
         for(int j = 0; j < 3; j++){
@@ -91,5 +102,21 @@ void mostrarMatriz(){
         }
         cout << endl;
     }
+    cout << "-------------------";
+    cout << endl;
 
+    cout << "Esa es su matriz cargada. \nDesea volver al menu interactivo?" << endl;
+    cout << "1. Si" << endl;
+    cout << "2. No" << endl;
+    cin >> volver;
+    
+    switch(volver){
+        case 1:
+            main();
+            break;
+        case 2:
+            break;
+    }
 }
+
+//BUSQUEDA DE UN VALOR INGRESADO POR EL USUARIO DENTRO DE LA MATRIZ
