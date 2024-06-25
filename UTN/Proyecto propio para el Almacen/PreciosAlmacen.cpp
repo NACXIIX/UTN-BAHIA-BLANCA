5eebeb3cc425ab8a055cc08fedb0fd684a7dea78
NACXIIX <nicocordano@hotmail.com>
2024-06-25 17:53:55 -0300

    Para el almacen, a ver si sirve, de paso practico

--- UTN/Proyecto propio para el Almacen/PreciosAlmacen.cpp
+++ UTN/Proyecto propio para el Almacen/PreciosAlmacen.cpp
@@ -2,15 +2,14 @@
 #include <sstream>
 #include <fstream>
 #include <string>
-
-#define coca "cocacola.csv"
+#define coca "cocacola.csv" //  En la def coca almacenamos la ruta del archivo.
 
 using namespace std;
 
-//Prototipos de funcion
+// Prototipos de funcion
 void LeerPrecios();
 
-
+// Programa principal
 int main(){
     LeerPrecios();
 
@@ -21,9 +20,23 @@ void LeerPrecios(){
     ifstream archivo(coca);
 
     string linea;
-    char delimitador = ',';
+    char delimitador = ','; // Este lo uso si quiero mostrar cada valor separado por coma
+    char saltoDeLinea = '\n'; // Este lo uso si quiero mostrar fila por fila.
+
     if (archivo.is_open()){
         getline(archivo,linea);
-    }
 
+        while (getline(archivo,linea)){
+            stringstream ss(linea);
+            string valor;
+
+            while (getline(ss,valor,saltoDeLinea)){
+                cout << "Valor: " << valor << endl;
+            }
+        }
+
+        archivo.close();
+    } else {
+        cout << "Hubo un error al intentar abrir el archivo.";
+    }
 }
