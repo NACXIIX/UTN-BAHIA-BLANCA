07e40a37529ab5c0e504d1719450a3e3b1567def
NACXIIX <nicocordano@hotmail.com>
2024-09-11 20:47:37 -0300

    Empezando ej 3 del TP 4 de Programacion II

---
+++ UTN/PROG II/TP 4/ej3/automovil.py
@@ -0,0 +1,7 @@
+class Automovil:
+    def __init__(self, marca:str, modelo:str, anio:int, velocidadMaxima:float, velocidadActual:float):
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__anio = anio
+        self.__velocidadMaxima = velocidadMaxima
+        self.__velocidadActual = velocidadActual
