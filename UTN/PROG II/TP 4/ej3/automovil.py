cbc8b8ce7f5d03974b8ede6b1a636dbdfdab34a2
NACXIIX <nicocordano@hotmail.com>
2024-09-12 11:17:29 -0300

    Terminando el ej 3 del TP 4 Programacion II

--- UTN/PROG II/TP 4/ej3/automovil.py
+++ UTN/PROG II/TP 4/ej3/automovil.py
@@ -1,7 +1,79 @@
 class Automovil:
+    # ATRIBUTOS DE INSTANCIA
     def __init__(self, marca:str, modelo:str, anio:int, velocidadMaxima:float, velocidadActual:float):
         self.__marca = marca
         self.__modelo = modelo
         self.__anio = anio
         self.__velocidadMaxima = velocidadMaxima
         self.__velocidadActual = velocidadActual
+
+    # COMANDOS
+    def establecerMarca(self, marca:str):
+        self.__marca = marca
+    
+    def establecerModelo(self, modelo:str):
+        self.__modelo = modelo
+
+    def establecerAnio(self, anio:int):
+        self.__anio = anio
+    
+    def establecerVelocidadMaxima(self, velocidadMax:float):
+        self.__velocidadMaxima = velocidadMax
+    
+    def establecerVelocidadActual(self, velocidad:float):
+        self.__velocidadActual = velocidad
+
+    def acelerar(self, incrementoVelocidad:int):
+        puede_acelerar = False
+        
+        if self.__velocidadActual < self.__velocidadMaxima and self.__velocidadActual < incrementoVelocidad + self.__velocidadActual:
+            self.__velocidadActual += incrementoVelocidad
+            if self.__velocidadActual > self.__velocidadMaxima:
+                self.__velocidadActual = self.__velocidadActual - (self.__velocidadActual - self.__velocidadMaxima)
+                return False # Funciona bien pero acelera, hasta la velocidad maxima. Buscar la forma de printear que acelero pero no del todo
+            puede_acelerar = True
+        else:
+            self.__velocidadActual = self.__velocidadActual - (self.__velocidadActual - self.__velocidadMaxima)
+            puede_acelerar = False
+        return puede_acelerar
+
+    def desacelerar(self, decrementoVelocidad:int):
+        puede_desaceler: False
+
+        if self.__velocidadActual > 0:
+            self.__velocidadActual -= decrementoVelocidad
+            if self.__velocidadActual < 0:
+                self.__velocidadActual = 0
+                return False #Funciona bien pero si desacelera, hasta 0. buscar la forma de printear que desacelero pero no del todo
+            puede_desaceler = True
+        else:
+            self.__velocidadActual = 0
+            puede_desaceler = False
+        
+        return puede_desaceler
+
+    def frenarPorCompleto(self):
+        auto_detenido = False
+        if self.__velocidadActual > 0:
+            self.__velocidadActual = 0
+            auto_detenido = True
+        return auto_detenido
+
+    #CONSULTAS
+    def obtenerMarca(self):
+        return f'{self.__marca}'
+    
+    def obtenerModelo(self):
+        return f'{self.__modelo}'
+
+    def obtenerAnio(self):
+        return f'{self.__anio}'
+
+    def obtenerVelocidadMaxima(self):
+        return f'{self.__velocidadMaxima}'
+
+    def obtenerVelocidadActual(self):
+        return f'{self.__velocidadActual}'
+
+    def calcularMinutosParaLlegar(self, distanciaKM:float)-> int:
+        pass
---
+++ UTN/PROG II/TP 4/ej3/test.py
@@ -0,0 +1,49 @@
+from automovil import Automovil
+
+class TestAutomovil:
+
+    @staticmethod
+    def test():
+        auto_1 = Automovil("Ford", "Focus", 2005, 210, 0)
+        print(f"El auto es: {auto_1.obtenerMarca()}")
+
+        if auto_1.acelerar(100):
+            print(f'El auto aceleró. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        else:
+            print(f'El auto no aceleró mas ya que llegó a su velocidad maxima. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        if auto_1.acelerar(100):
+            print(f'El auto aceleró. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        else:
+            print(f'El auto no aceleró mas ya que llegó a su velocidad maxima. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        if auto_1.acelerar(100):
+            print(f'El auto aceleró. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        else:
+            print(f'El auto no aceleró mas ya que llegó a su velocidad maxima. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        if auto_1.acelerar(100):
+            print(f'El auto aceleró. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        else:
+            print(f'El auto no aceleró mas ya que llegó a su velocidad maxima. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+
+        if auto_1.desacelerar(100):
+            print(f'El auto desacelero. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        else:
+            print (f'El auto no desacelero. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        if auto_1.desacelerar(100):
+            print(f'El auto desacelero. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        else:
+            print (f'El auto no desacelero. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        if auto_1.desacelerar(100):
+            print(f'El auto desacelero. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        else:
+            print (f'El auto no desacelero. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        if auto_1.desacelerar(100):
+            print(f'El auto desacelero. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        else:
+            print (f'El auto no desacelero. Velocidad actual: {auto_1.obtenerVelocidadActual()}')
+        
+        auto_1.frenarPorCompleto()
+        auto_1.acelerar(120)
+        auto_1.frenarPorCompleto()
+        print (auto_1.obtenerVelocidadActual())
+if __name__ == '__main__':
+    TestAutomovil.test()
